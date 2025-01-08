5a4c78e1bc6f7d27472c021a7593130307e76457
farhana786W <shaikfarhana343@gmail.com>
2025-01-08 18:54:33 +0530

    Update StringMethods.py

---
+++ Strings/StringMethods.py
@@ -0,0 +1,10 @@
+# Different ways of declaring the Strings
+
+s1 = 'My Name is Farhana'
+s2 = "My Age is 25"
+s3 = """We are Learning Strings"""
+s4 = ''''Python is Dynamically Typed Language'''
+print(s1)
+print(s2)
+print(s3)
+print(s4)
